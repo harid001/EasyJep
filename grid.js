51a70086cc7074580b30767cf754ca16a22dd67f
Hari Dhanushkodi <hari97.nd@gmail.com>
2015-09-10 15:16:40 -0700

    50% grid sys complete

--- grid.js
+++ grid.js
@@ -1,9 +1,30 @@
 addEventListener('load', function() {
-  var row = document.querySelector('.row');
-  for(var i = 0; i < sessionStorage.categories; i++){
-    var block = document.createElement('div');
-    block.className = 'block';
-    row.appendChild(block);
-  }
+
+  var MARGIN_LEFT_RIGHT = 0.5;
+  var HEIGHT = 7;
+
+  (function (categories, max_score) {
+    var row = document.querySelector('.row');
+    var num_margins = 2 * (categories-1) + 2;
+    var width = (98 - num_margins * MARGIN_LEFT_RIGHT) / categories;
+    var margin_top_bottom = (100 - categories * HEIGHT) / num_margins ;
+
+    for(var i = 0; i < categories; i++){
+      for(var j = 0; j < categories; j++){
+        var block = document.createElement('div');
+        block.className = 'block';
+        block.style.width = String(width) + 'vw';
+        block.style.height = String(HEIGHT) + 'vh';
+        block.style.marginLeft = MARGIN_LEFT_RIGHT + 'vw';
+        block.style.marginRight = MARGIN_LEFT_RIGHT + 'vw';
+        block.style.marginTop = margin_top_bottom + 'vh';
+        block.style.marginBottom = margin_top_bottom + 'vh';
+        row.appendChild(block);
+      }
+    }
+
+
+  })(sessionStorage.categories, sessionStorage.max_score);
+
 
 });
--- validation.js
+++ validation.js
@@ -1,26 +1,26 @@
 addEventListener('load', function() {
-  var startButton = document.querySelector("#startButton"),
-    categoriesInput = document.querySelector('#categories'),
-    maxScoreInput = document.querySelector('#maxScore'),
+  var start_button = document.querySelector("#startButton"),
+    categories_input = document.querySelector('#categories'),
+    max_score_input = document.querySelector('#maxScore'),
     categories = null,
-    maxScore = null;
+    max_score = null;
 
-  startButton.addEventListener('click', function() {
-    categories = categoriesInput.value;
-    maxScore = maxScoreInput.value;
+  start_button.addEventListener('click', function() {
+    categories = categories_input.value;
+    max_score = max_score_input.value;
 
-    if(/^\d+$/.test(categories) && /^\d+$/.test(maxScore)){
+    if(/^\d+$/.test(categories) && /^\d+$/.test(max_score)){
       categories = Number(categories);
-      maxScore = Number(maxScore);
+      max_score = Number(max_score);
 
       if(categories < 25){
         sessionStorage.categories = categories;
-        sessionStorage.maxScore = maxScore;
+        sessionStorage.max_score = max_score;
         window.location.href = 'grid.html'; // --> i have no idea
       }
     }
     else {
-      // replace line with bettet alert system
+      // replace line with better alert system
       alert('Invalid');
     }
   });
