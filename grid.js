b668dd49abfb21b1430bb255c35531591c0ecadf
Hari Dhanushkodi <hari97.nd@gmail.com>
2015-09-22 19:48:11 -0700

    prepare for grid fix

--- grid.js
+++ grid.js
@@ -5,7 +5,7 @@ addEventListener('load', function() {
      reverse_nav = function(reverse) {
 
        return function() {
-         colors1 = ['#FFFFFF', '#FF961F'];
+         colors1 = ['#FFFFFF', '#5B5D5E'];
          colors2 = ['#4D94FF', '#FFFFFF'];
          navbar.style.backgroundColor = colors2[1 - reverse];
          for(var i = 0; i < navbar_row.children.length; i++){
@@ -19,39 +19,33 @@ addEventListener('load', function() {
   navbar.addEventListener('mouseout', reverse_nav(1));
 
 
+  (function (categories, max_score) {
+    var width = 80 / categories;
+    var height = 80 / (max_score / 100);
+    var num_margins = 2 * categories;
+    var margin_left_right = (100 - categories * width) / num_margins;
+    var margin_top_bottom = (100 - (max_score / 100) * height) / (2 * max_score / 100);
 
+    for(var i = 0; i < max_score / 100; i++){
+      var row = document.querySelector('#row' + i);
+      for(var j = 0; j < categories; j++){
+        var block = document.createElement('div');
+        block.className = 'block';
+        block.style.backgroundColor = (i === 0 || j === 0) ? '#000000' : block.style.backgroundColor;
+        block.style.width = String(width) + 'vw';
+        block.style.height = String(height) + 'vh';
+        block.style.marginLeft = margin_left_right + 'vw';
+        block.style.marginRight = margin_left_right + 'vw';
+        block.style.marginTop = margin_top_bottom + 'vh';
+        block.style.marginBottom = margin_top_bottom + 'vh';
+        row.appendChild(block);
+      }
+      var newRow = document.createElement('div');
+      newRow.setAttribute('id','row' + (i+1));
+      document.body.appendChild(newRow);
+    }
 
-
-  // create the grid
-  // (function (categories, max_score) {
-  //   var width = 80 / categories;
-  //   var height = 80 / (max_score / 100);
-  //   var num_margins = 2 * categories;
-  //   var margin_left_right = (100 - categories * width) / num_margins;
-  //   var margin_top_bottom = (100 - (max_score / 100) * height) / (2 * max_score / 100);
-  //
-  //   for(var i = 0; i < max_score / 100; i++){
-  //     var row = document.querySelector('#row' + i);
-  //     for(var j = 0; j < categories; j++){
-  //       var block = document.createElement('div');
-  //       block.className = 'block';
-  //       block.style.backgroundColor = (i === 0 || j === 0) ? '#000000' : block.style.backgroundColor;
-  //       block.style.width = String(width) + 'vw';
-  //       block.style.height = String(height) + 'vh';
-  //       block.style.marginLeft = margin_left_right + 'vw';
-  //       block.style.marginRight = margin_left_right + 'vw';
-  //       block.style.marginTop = margin_top_bottom + 'vh';
-  //       block.style.marginBottom = margin_top_bottom + 'vh';
-  //       row.appendChild(block);
-  //     }
-  //     var newRow = document.createElement('div');
-  //     newRow.setAttribute('id','row' + (i+1));
-  //     document.body.appendChild(newRow);
-  //   }
-  //
-  // })(Number(sessionStorage.categories) + 1, Number(sessionStorage.max_score) + 100);
-
-
+  })(Number(sessionStorage.categories) + 1, Number(sessionStorage.max_score) + 100);
 
 
 });
